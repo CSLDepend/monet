965b57f7b9c5d4d06546672bcadf46f1b3466dfa
apatke <apatke@illinois.edu>
2019-07-10 12:32:49 -0500

    Remove duplicated directory

--- .ipynb_checkpoints/create_docker-checkpoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# docker build -t monet_pcl:latest -f ./Dockerfile_pcl.base .
-docker build -t monet_spark:latest -f ./Dockerfile_spark.base .
--- src/.ipynb_checkpoints/unix_timestamp-checkpoint.py
+++
@@ -1,6 +0,0 @@
-import time
-import datetime
-def get_unix_time(s):
-    s = str(s)
-    return int(time.mktime(datetime.datetime.strptime(s, "%Y%m%d").timetuple()))
-
