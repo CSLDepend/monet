c3241ee8e447e59575fd28920119e110ae82fd2a
apatke <44070584+apatke@users.noreply.github.com>
2019-08-11 11:24:49 -0500

    Update processing_pcl.py

--- src/processing_pcl.py
+++ src/processing_pcl.py
@@ -1,5 +1,12 @@
 #run as python3 date
 #import python libraries
+
+
+# Copyright (c) 2019 DEPEND Research Group at
+# University of Illinois, Urbana Champaign (UIUC)
+# This work is licensed under the terms of the UIUC/NCSA license.
+# For a copy, see https://opensource.org/licenses/NCSA.
+
 import os
 import sys
 from unix_timestamp import *
