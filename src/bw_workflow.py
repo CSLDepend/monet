097412c05e26a846200402fbb9d2550489f5e37e
apatke <44070584+apatke@users.noreply.github.com>
2019-08-11 11:26:07 -0500

    Update bw_workflow.py

--- src/bw_workflow.py
+++ src/bw_workflow.py
@@ -1,3 +1,9 @@
+# Copyright (c) 2019 DEPEND Research Group at
+# University of Illinois, Urbana Champaign (UIUC)
+# This work is licensed under the terms of the UIUC/NCSA license.
+# For a copy, see https://opensource.org/licenses/NCSA.
+
+
 #run as python3 date timestamp parquetDir pcdDir
 #import python libraries
 import time
