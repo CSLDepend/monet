fc968335c894b267ebb85311cbd2e724175935c9
apatke <44070584+apatke@users.noreply.github.com>
2019-08-11 11:22:17 -0500

    Update create_docker.sh

--- create_docker.sh
+++ create_docker.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+
+# Copyright (c) 2019 DEPEND Research Group at
+# University of Illinois, Urbana Champaign (UIUC)
+# This work is licensed under the terms of the UIUC/NCSA license.
+# For a copy, see https://opensource.org/licenses/NCSA.
+
 docker build -t monet_pcl:latest -f ./Dockerfile_pcl.base .
 docker build -t monet_spark:latest -f ./Dockerfile_spark.base .
