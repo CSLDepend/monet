35689dea161e5fcfe0e1f1102bb85fb61d80d16c
apatke <44070584+apatke@users.noreply.github.com>
2019-08-13 17:20:05 -0500

    Update build_PCL.sh

--- build_PCL.sh
+++ build_PCL.sh
@@ -1,1 +1,6 @@
+# Copyright (c) 2019 DEPEND Research Group at
+# University of Illinois, Urbana Champaign (UIUC)
+# This work is licensed under the terms of the UIUC/NCSA license.
+# For a copy, see https://opensource.org/licenses/NCSA.
+
 docker run --rm  -v `pwd`:/monet/ -ti monet_pcl:latest /bin/bash -c "cd /monet/src/pcl; mkdir build; cd build; cmake ..; make; cp rgselect_unique ../../rgselect_unique"
